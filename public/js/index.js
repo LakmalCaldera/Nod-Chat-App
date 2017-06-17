7df3202b8b47041b64bb28f01d6045928ca60b80
Lakmal Caldera <lakmal.developer@gmail.com>
2017-06-17 23:22:47 +0530

    added auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to Server');
 });
@@ -25,6 +41,7 @@ socket.on('newMessage', function(data){
     from: data.from
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(data){
@@ -46,9 +63,7 @@ socket.on('newLocationMessage', function(data){
     from: data.from
   });
   jQuery('#messages').append(html);
-
-
-
+  scrollToBottom();
 });
 
 function logger(messageData){
